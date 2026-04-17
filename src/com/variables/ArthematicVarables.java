db01ef0bd2a6aa6b51130f6ecd8abf6cb9fa5f7a
SURYA <mandapatisurya4579@gmail.com>
2026-04-17 08:19:13 +0530

    variables

---
+++ src/com/variables/ArthematicVarables.java
@@ -0,0 +1,34 @@
+package com.variables;
+
+public class ArthematicVarables {
+	public static void main(String[] args) {
+		int a = 15, b = 4;
+		int c = a + b;
+		int e = a - b;
+		int f = a * b;
+		int g = a / b;
+		int h = a % b;
+		boolean i = a > b;
+		boolean j = a == b;
+		
+		System.out.println("a+b:"+c);
+		System.out.println("a-b:"+e);
+		System.out.println("a*b:"+f);
+		System.out.println("a/b:"+g);
+		System.out.println("a%b:"+h);
+		System.out.println("a>b:"+i);
+		System.out.println("a==b:"+j);
+		
+		System.out.println();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a>b);
+		System.out.println(a==b);
+		
+		
+	}
+}
